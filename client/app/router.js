17a6a96d3b5334503bfd3ad5a730e843cb11a9fc
mize85 <Mize85@gmx.net>
2015-06-07 16:46:51 +0200

    Initial commit

---
+++ client/app/router.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('user');
+  this.route('userInput');
+
+  this.route("login");
+  this.route("register");
+});
+
+export default Router;
---
+++ server/api/controllers/UserInputController.js
@@ -0,0 +1,11 @@
+/**
+ * UserInputController
+ *
+ * @description :: Server-side logic for managing userinputs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
