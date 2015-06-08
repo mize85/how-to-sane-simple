01cac3a222fd2af64252d032a4a91746c2226adb
LarsBuenting <l.buenting@e-patrol.de>
2015-06-08 17:45:48 +0200

    removed login controller mixin (deprecated)

--- client/app/pods/components/login/login-panel/component.js
+++ client/app/pods/components/login/login-panel/component.js
@@ -1,6 +1,23 @@
 import Ember from 'ember';
-import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
-export default Ember.Component.extend(LoginControllerMixin, {
-  authenticator: 'simple-auth-authenticator:oauth2-password-grant'
+export default Ember.Component.extend({
+    authenticator: 'simple-auth-authenticator:oauth2-password-grant',
+
+    actions: {
+        /**
+         This action will authenticate the session with the configured
+         authenticator (see
+         if both `identification` and `password` are non-empty. It passes both
+         values to the authenticator.
+         __The action also resets the `password` property so sensitive data does
+         not stay in memory for longer than necessary.__
+         @method actions.authenticate
+         */
+        authenticate: function() {
+            var options = this.getProperties('identification', 'password');
+            this.set('password', null);
+            var authenticator = this.get('authenticator');
+            return this.get('session').authenticate(authenticator, options);
+        }
+    }
 });
